91831642e897d6b0cb415fa7552ed9644bdb9c15
DAnn2012 <dannunziata@hotmail.it>
2025-10-04 21:39:39 +0200

    Update class-eb-post-types.php

--- edwiser-bridge/includes/class-eb-post-types.php
+++ edwiser-bridge/includes/class-eb-post-types.php
@@ -261,7 +261,7 @@ class Eb_Post_Types {
 		if ( 'eb_order' === $args['args']['post_type'] ) {
 			$css_class = 'eb-wdm-order-meta';
 			echo '<strong>';
-			echo esc_html__( 'Order ', 'edwiser-bridge' ) . esc_html( printf( '#%s ', get_the_id() ) ) . esc_html__( 'Details ', 'edwiser-bridge' );
+			echo esc_html( sprintf( __( 'Order #%s Details', 'edwiser-bridge' ), get_the_id() ) );
 			echo '</strong>';
 			echo "<div id='" . esc_html( $args['args']['post_type'] ) . "'_options' class='post-options " . esc_html( $css_class ) . "'>";
 		} else {
